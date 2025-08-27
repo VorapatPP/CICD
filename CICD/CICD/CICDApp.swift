bef09efef6ecc437c61f688027f71783c2b13d93
Vorapat Phorncharroenroj <vorapat@Vorapats-MacBook-Pro.local>
2025-08-28 03:20:33 +0700

    init

---
+++ CICD/CICD/CICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDApp.swift
+//  CICD
+//
+//  Created by Vorapat Phorncharroenroj on 28/8/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct CICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
